4f9ab016f64da568a45f48744be04b2d188a4d37
Donald Belliveau <donaldsbelliveau@gmail.com>
2018-01-25 17:02:52 -0800

    Moved ViewController file to Controllers folder.  Renamed to ContentsVC.  General code cleanup.

--- Modern Hamlet/Controllers/ContentsVC.swift
+++ Modern Hamlet/Controllers/ContentsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ContentsVC.swift
 //  Modern Hamlet
 //
 //  Created by Donald Belliveau on 2018-01-25.
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ContentsVC: UIViewController {
+    
+    // Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
+    
+    
+} // END Class.
 
