5c6539f971b702852684f4e30bfbccbd9bd46ba4
Donald Belliveau <donaldsbelliveau@gmail.com>
2018-01-25 16:45:52 -0800

    Initial Commit

---
+++ Modern Hamlet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Modern Hamlet
+//
+//  Created by Donald Belliveau on 2018-01-25.
+//  Copyright © 2018 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
